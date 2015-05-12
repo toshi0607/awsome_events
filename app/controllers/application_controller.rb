4918b68fe8eb5d7c9d46d789f14101c827a9aedf
Tshinori Sugita <sugita@c-fo.com>
2015-05-12 10:18:08 +0900

    add current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
 
   private
 
+ def current_user
+   return unless session[:user_id]
+   @current_user ||= User.find(session[:user_id])
+ end
+
   def logged_in?
     !!session[:user_id]
   end
