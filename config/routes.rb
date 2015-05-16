e1f1bf0e6531b270051437ea243cf43917323031
Tshinori Sugita <sugita@c-fo.com>
2015-05-16 12:39:52 +0900

    add participation button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :events
-
   root to: "welcome#index"
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
+
+  resources :events do
+    resources :tickets
+  end
 end
