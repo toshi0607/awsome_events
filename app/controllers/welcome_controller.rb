ed1c5f63ab18299adde5965c39eb0df16debd39c
Tshinori Sugita <sugita@c-fo.com>
2015-05-17 21:56:01 +0900

    restrict search params

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,13 +4,13 @@ class WelcomeController < ApplicationController
   def index
     @q = Event.page(params[:page]).per(PER).order(:start_time).search(search_params)
 
-    @events = @q.result(distinct: true)
+    @events = @q.result
   end
 
   private
 
   def search_params
-    params.require(:q).permit!
+    params.require(:q).permit(:name_cont, :start_time_gteq)
   rescue
     { start_time_gteq: Time.zone.now }
   end
